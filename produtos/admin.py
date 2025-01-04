333261bd12486f1c5dc8be3a2eeb7e63cb43ca6a
LucasAMiranda <lucasa.miranda@hotmail.com>
2025-01-04 17:04:23 -0300

    admin de produto atualizado com sucesso

--- produtos/admin.py
+++ produtos/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Produto
+
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'descricao', 'marca', 'quantidade_minima', 'quantidade', 'custo', 'preco', 'observacao')
+
+
+admin.site.register(Produto, ProdutoAdmin)
 
 # Register your models here.
