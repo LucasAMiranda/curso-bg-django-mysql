563c3fb57783b8835115d1eaf014d1e9dbf4aca9
LucasAMiranda <lucasa.miranda@hotmail.com>
2025-02-11 19:10:50 -0300

    Fazendo alteração em clientes com sucesso

--- clientes/urls.py
+++ clientes/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.lista_clientes, name='lista_clientes'),
     path('cadastrar/', views.cadastrar_cliente, name='cadastrar_cliente'),
     path('consultar_cliente/<str:cpf>/', views.consultar_cliente, name='consultar_cliente'),
-    path('excluir_cliente/<str:cpf>/', views.excluir_cliente, name='excluir_cliente'), 
+    path('excluir_cliente/<str:cpf>/', views.excluir_cliente, name='excluir_cliente'),
 ]
--- clientes/views.py
+++ clientes/views.py
@@ -1,57 +1,55 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse
 from .models import Cliente
 from .forms import ClienteForm
-from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 @login_required
-#lietar e cadastrar Clientes
 
 def lista_clientes(request):
     clientes = Cliente.objects.all()
     return render(request, 'lista_clientes.html', {'clientes': clientes})
-
+    
 
 def cadastrar_cliente(request):
     if request.method == 'POST':
         cpf = request.POST.get('cpf')
-        cliente = cliente.objects.filter(cpf=cpf).first()
-
+        cliente = Cliente.objects.filter(cpf=cpf).first()
         if cliente:
-            #Atualizar cliente existente
+            # Atualizar cliente existente
             form = ClienteForm(request.POST, instance=cliente)
         else:
-            #Criar novo cliente
+        # Criar novo cliente
             form = ClienteForm(request.POST)
-
-        if form.is_valid():
-            form.save()
-            return redirect('lista_clientes')
+            if form.is_valid():
+                form.save()
+                return redirect('lista_clientes')
     else:
-        form = ClienteForm
-    
-    return render(request, 'cadastrar_cliente.html', {'form':form})
+        form = ClienteForm()
+        return render(request, 'cadastrar_cliente.html', {'form': form})
 
-def consultar_cliente():
-    try:
-        #remove qualquer formatação de cpf antes de buscar 
+
+def consultar_cliente(request, cpf):
+   try:
+    # Remove qualquer formatação do CPF antes de buscar
         cpf = ''.join(filter(str.isdigit, cpf))
         cliente = Cliente.objects.get(cpf=cpf)
         data = {
-            'nome': cliente.nome,
-            'telefone': cliente.telefone,
-            'email': cliente.email
+        'nome': cliente.nome,
+        'telefone': cliente.telefone,
+        'email': cliente.email,
         }
         return JsonResponse(data)
-    except Cliente.DoesNotExist:
-        return JsonResponse({'error': 'Cliente não encontrado.'}, status=404)
+
+   except Cliente.DoesNotExist:
+    return JsonResponse({'error': 'Cliente não encontrado.'}, status=404)
+
 
 def excluir_cliente(request, cpf):
     if request.method == 'DELETE':
-        try:
-            cliente = Cliente.objects.get(cpf=cpf)
-            cliente.delete()
-            return JsonResponse({'message': 'Cliente excluído com sucesso.'})
-        except Cliente.DoesNotExist:
-            return JsonResponse({'error': 'Cliente não econtrado.'}, status=404)
+     try:
+        cliente = Cliente.objects.get(cpf=cpf)
+        cliente.delete()
+        return JsonResponse({'message': 'Cliente excluído com sucesso.'})
+     except Cliente.DoesNotExist:
+        return JsonResponse({'error': 'Cliente não encontrado.'}, status=404)
