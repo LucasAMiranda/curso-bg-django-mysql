f103e18c3d3775b04dd6c61d16e2dde5da4e3fb3
LucasAMiranda <lucasa.miranda@hotmail.com>
2025-03-30 12:31:28 -0300

    Banco de Dados configurando MySQL

--- meu_projeto/settings.py
+++ meu_projeto/settings.py
@@ -92,11 +92,15 @@ WSGI_APPLICATION = 'meu_projeto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'comercial',
+    'USER': 'root',
+    'PASSWORD': '',
+    'HOST': 'localhost',
+    'PORT': '3306',
     }
-}
 
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
